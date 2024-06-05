11b4793ea1500cbbd364a8860aba4e5183e7f65a
Benjaminfoley <foleybe@oregonstate.edu>
2024-06-05 12:55:37 -0700

    template finished for the assingments API

--- api/assignments.js
+++ api/assignments.js
@@ -2,8 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 // Placeholder Route to ensure server is working
-router.get('/', (req, res) => {
+router.get('/assignments/:id', (req, res) => {
   res.json({ message: 'Assignments route placeholder' });
+});
+
+router.post('/assignments', (req, res) => {
+  res.json({ message: 'Assignment created' });
+});
+
+router.patch('/assignments/:id', (req, res) => {
+  res.json({ message: 'Assignment updated' });
+});
+
+router.delete('/assignments/:id', (req, res) => {
+  res.json({ message: 'Assignment deleted' });
+});
+
+router.get('/assignments/:id/submissions', (req, res) => {
+  res.json({ message: 'Submissions route placeholder' });
+});
+
+router.post('/assignments/:id/submissions', (req, res) => {
+  res.json({ message: 'Submission created' });
 });
 
 module.exports = router;
