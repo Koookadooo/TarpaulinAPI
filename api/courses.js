66cfcdcb8c55d87a30459e92c4c73a186e1907d9
Daniel <neffd@oregonstate.edu>
2024-06-05 12:58:59 -0700

    Added basic endpoints for courses

--- api/courses.js
+++ api/courses.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
   res.json({ message: 'Courses route placeholder' });
 });
 
+router.post('/', (req, res, next) => {
+  res.json({ message: "POST /courses" })
+});
+
+router.get('/:id', (req, res, next) => {
+  res.json({ message: `GET /courses/${req.params.id}` })
+});
+
+router.patch('/:id', (req, res, next) => {
+  res.json({ message: `PATCH /courses/${req.params.id}` })
+});
+
+router.delete('/:id', (req, res, next) => {
+  res.json({ message: `DELETE /courses/${req.params.id}` })
+});
+
 module.exports = router;
