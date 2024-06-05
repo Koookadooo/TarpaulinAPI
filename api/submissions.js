fc5defb0f6ad35494039d775fe86a31ff468a552
Daniel <neffd@oregonstate.edu>
2024-06-05 12:46:07 -0700

    removed submissions.js

--- api/submissions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder Route to ensure server is working
-router.get('/', (req, res) => {
-  res.json({ message: 'Submissions route placeholder' });
-});
-
-module.exports = router;
