c3e2ffc88ac8220859dd73d52fc8614fa15c2f86
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-06-07 10:34:11 -0700

    user endpoints add error checking

--- api/users.js
+++ api/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const { requireAuth, generateAuthToken } = require('../lib/auth');
 const { User, UserClientFields } = require('../models/user');
@@ -9,11 +10,11 @@ const { ValidationError } = require('sequelize');
  */
 router.post('/users', async function (req, res, next) {
   try {
-    const user = await User.create(req.body, UserClientFields);
+    const user = await User.create(req.body, { fields: UserClientFields });
     res.status(201).json(user);
   } catch (err) {
     if (err instanceof ValidationError) {
-      res.status(400).json({ error: 'Validation error' });
+      res.status(400).json({ error: 'Validation error', details: err.errors });
     } else {
       next(err);
     }
@@ -25,26 +26,38 @@ router.post('/users', async function (req, res, next) {
  */
 
 router.post('/users/login', async function (req, res, next) {
-  const user = await User.findOne({ where: { email: req.body.email }});
-  const authenticated = await bcrypt.compare(req.body.password, user.password);
-  if (authenticated) {
-    const token = generateAuthToken(user.userId);
-    res.status(200).json({ "token": token });
-  } else {
-    res.status(401).json({ error: 'Invalid login' });
+  try {
+    const user = await User.findOne({ where: { email: req.body.email }});
+    if (user) {
+      const authenticated = await bcrypt.compare(req.body.password, user.password);
+      if (authenticated) {
+        const token = generateAuthToken(user.userId);
+        res.status(200).json({ "token": token });
+      } else {
+        res.status(401).json({ error: 'Invalid login' });
+      }
+    } else {
+      res.status(401).json({ error: 'Invalid login' });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
 /*
  * Route to fetch data for a specific user.
  */
-router.get('/users/:userId', requireAuth, async function (req, res) {
-  const userId = req.params.userId;
-  const user = await User.findOne({ where: { userId } });
-  if (user) {
-    res.status(200).json(user);
-  } else {
-    res.status(404).json({ error: 'User not found' });
+router.get('/users/:userId', requireAuth, async function (req, res, next) {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findOne({ where: { userId } });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (err) {
+    next(err);
   }
 }),
 
