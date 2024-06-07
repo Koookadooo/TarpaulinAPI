56c0ad8384df1fe9e0c8c82d5807467ee68a39e1
Benjaminfoley <foleybe@oregonstate.edu>
2024-06-07 12:13:57 -0700

    New branch ablity to get all assignments with GET req

--- api/assignments.js
+++ api/assignments.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { Assignment,AssignmentClientFields } = require('../models/assignment');
+const {ValidationError} = require('sequelize');
 
-// Placeholder Route to ensure server is working
-router.get('/', (req, res) => {
-  res.json({ message: 'Assignments route placeholder' });
+/* 
+  *GET assignments listing.
+*/
+router.get('/', async function (req, res) {
+
+  let page = parseInt(req.query.page) || 1
+  page = page < 1 ? 1 : page
+  const numPerPage = 10
+  const offset = (page - 1) * numPerPage
+
+  const result = await Assignment.findAndCountAll({
+    limit: numPerPage,
+    offset: offset
+  });
+
+  const lastPage = Math.ceil(result.count / numPerPage)
+  const links = {}
+  if (page < lastPage) {
+    links.nextPage = `/assignments?page=${page + 1}`
+    links.lastPage = `/assignments?page=${lastPage}`
+  }
+  if (page > 1) {
+    links.prevPage = `/assignments?page=${page - 1}`
+    links.firstPage = '/assignments?page=1'
+  }
+
+  res.status(200).json({
+    assignments: result.rows,
+    pageNumber: page,
+    totalPages: lastPage,
+    pageSize: numPerPage,
+    totalCount: result.count,
+    links: links
+  });
 });
-
 module.exports = router;
