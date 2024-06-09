de102b1322ab0715b187c2d646f04bd5feaf917b
Benjaminfoley <foleybe@oregonstate.edu>
2024-06-08 20:36:30 -0700

    Working on getting instructors auth to post an assignment

--- api/assignments.js
+++ api/assignments.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {requireAuth, requireRole} = require('../lib/auth');
+const {Course} = require('../models/course');
+const {User} = require('../models/user');
 const { Assignment,AssignmentClientFields } = require('../models/assignment');
 const {ValidationError} = require('sequelize');
 
@@ -54,18 +57,60 @@ router.get('/:id', async function (req, res, next) {
 /*
   *POST create assignment.
 */
-router.post('/', async function (req, res,next) {
+// router.post('/',requireAuth,requireRole('admin'), async function (req, res,next) {
+//   try {
+//     const user = req.user;
+//     if(user.role != 'admin'){
+//       return res.status(403).send({ error: "Forbidden: Admin access required" }); 
+//     }
+
+//     const existingCourse = await Course.findByPk(req.body.assignmentId);
+//     if(existingCourse){
+//       return res.status(400).send({ error: "FAILURE: Assignment already exists" });
+//     }
+
+//     const assignment = await Assignment.create(req.body, AssignmentClientFields);
+//     res.status(201).json(assignment); 
+//   }catch (err) {
+//     if (err instanceof ValidationError) {
+//       res.status(400).json({ message: err.errors[0].message });
+//     } else {
+//       next(err);
+//     }
+//   }
+// });
+
+router.post('/', requireAuth, requireRole('admin','instructor'), async function (req, res, next) {
   try {
+    const courseId = req.params.id;
+    const { title, points, due } = req.body;
+    const course = await Course.findByPk(courseId);
+    const user = await User.findByPk(req.user.id);
+
+    if (user.role !== "admin" && user.id !== course.instructorId) {
+      return res.status(403).send({ error: "Forbidden: Admin access required" });
+    }
+
+    const existingAssignment = await Assignment.findOne({ where: {title, points, due } });
+    if (existingAssignment) {
+      return res.status(400).send({ error: "FAILURE: Assignment already exists" });
+    }
+
     const assignment = await Assignment.create(req.body, AssignmentClientFields);
-    res.status(201).json(assignment);
-  } catch (err) {
-    if (err instanceof ValidationError) {
-      res.status(400).json({ message: err.errors[0].message });
-    } else {
-      next(err);
+    res.status(201).send({ message: "SUCCESS: Course Assignment", id: assignment.id });
+  }
+  catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: "FAILURE: " + e.message });
+    }
+    else {
+      throw e;
     }
   }
 });
+
+
+
 
 /*
   *PUT update assignment.
